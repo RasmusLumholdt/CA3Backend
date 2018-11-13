422ebd6d45534509338f02fc013186844dd1de17
RasmusLumholdt <dev.rlumholdt@gmail.com>
2018-11-13 21:03:37 +0100

    USers can now be created along with roles

---
+++ src/main/java/test/CreateUsers.java
@@ -0,0 +1,23 @@
+
+package test;
+
+import entity.User;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CreateUsers {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpapu");
+        EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        User u = new User("Test", "1234");
+        em.persist(u);
+        em.getTransaction().commit();
+        
+        
+        
+    }
+}
