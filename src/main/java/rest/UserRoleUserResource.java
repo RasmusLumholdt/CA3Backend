55abba2a4e075eae4fcb2287d36db40d25eedbfe
MartinMoller <martinmoellerzbc@gmail.com>
2018-11-18 14:32:18 +0100

    fixed roles endpoint

--- src/main/java/rest/UserRoleUserResource.java
+++ src/main/java/rest/UserRoleUserResource.java
@@ -5,38 +5,28 @@
  */
 package rest;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * REST Web Service
  *
  * @author wicktus
  */
-@Path("UserRoleUser")
-@RolesAllowed("user")
+@Path("roles")
 public class UserRoleUserResource {
 
     @Context
     private UriInfo context;
-    private ExecutorService pool;
 
-    /**
-     * Creates a new instance of UserRoleUserResource
-     */
-    public UserRoleUserResource() {
-        this.pool = Executors.newCachedThreadPool();
-    }
+    @Context
+    SecurityContext securityContext;
 
     /**
      * Retrieves representation of an instance of rest.UserRoleUserResource
@@ -44,20 +34,21 @@ public class UserRoleUserResource {
      * @return a response containing the appropriate JSON
      */
     @GET
+    @Path("user")
+    @RolesAllowed("user")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJSON() {
-        //TODO return proper representation object
-        return "hello!";
+    public String getUser() {
+        String user = securityContext.getUserPrincipal().getName();
+        return "\"Hello from USER: " + user + "\"";
+    }
+    
+    @GET
+    @Path("admin")
+    @RolesAllowed("admin")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAdmin() {
+        String user = securityContext.getUserPrincipal().getName();
+        return "\"Hello from USER: " + user + "\"";
     }
 
-    /**
-     * PUT method for updating or creating an instance of UserRoleUserResource
-     *
-     * @param content representation for the resource
-     * @return a success response
-     */
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
-    }
 }
